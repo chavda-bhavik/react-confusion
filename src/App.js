b2c98da3e6209a3c14d7b3e7d3da52efcc373bea
chavda-bhavik <bhavik.chvda@gmail.com>
2020-01-16 22:32:33 +0530

    message component part-1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap'
+import Menu from './components/MenuComponent'
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
