b24599ee0ac11bee53990f04d19bff68aeba4745
chavda-bhavik <bhavik.chvda@gmail.com>
2020-01-27 20:51:50 +0530

    added ContactComponent with Home Component Details

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+
 import { DISHES } from './../shared/dishes'
-import DishDetails from './DishDetailsComponent'
+import { COMMENTS } from './../shared/comments'
+import { LEADERS } from './../shared/leaders'
+import { PROMOTIONS } from './../shared/promotions'
+
+import Contact from './ContactComponent'
 import Menu from './MenuComponent'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
@@ -10,11 +15,14 @@ import { Switch, Route, Redirect } from "react-router-dom";
 class Main extends Component {
     state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS,
         selectedDish: null
     }
     render() {
         const HomePage = () => {
-            return <Home />
+            return <Home dish={this.state.dishes.filter( dish => dish.featured === true )[0]} promotion={this.state.promotions.filter( promo => promo.featured === true)[0]} leader={this.state.leaders.filter( leader => leader.featured===true)[0]} />
         }
         return (
         <div className="App">
@@ -22,6 +30,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
             <Footer />
